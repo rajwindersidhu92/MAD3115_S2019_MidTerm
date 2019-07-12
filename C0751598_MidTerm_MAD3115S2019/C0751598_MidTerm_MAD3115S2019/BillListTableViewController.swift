0c0d2e40c4a8c02518e67f6ec780d806869bec9b
rajwindersidhu92 <kaurrajwinder203@gmail.com>
2019-07-12 14:21:42 -0400

    table view customer

--- C0751598_MidTerm_MAD3115S2019/C0751598_MidTerm_MAD3115S2019/BillListTableViewController.swift
+++ C0751598_MidTerm_MAD3115S2019/C0751598_MidTerm_MAD3115S2019/BillListTableViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
-class BillListTableViewController: UIViewController {
+class BillListTableViewController: UIViewController, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    var data = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        
+        for i in 0...100 {
+            data.append("\(i)")
+        }
         // Do any additional setup after loading the view.
     }
     
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellCustomer")!
+        
+        let text = data[indexPath.row]
+        
+        cell.textLabel?.text = text
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
